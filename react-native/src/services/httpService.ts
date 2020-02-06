880f172c9f77f38bf76a4e5d3cfaa8cfba3ad439
yasir.aktunc <yasir.aktunc@obss.com.tr>
2020-02-07 00:22:15 +0300

    Rouer config is done and added header left button

--- react-native/src/services/httpService.ts
+++ react-native/src/services/httpService.ts
@@ -1,81 +1,80 @@
+import axios from 'axios'
+import NetInfo, { NetInfoState } from '@react-native-community/netinfo'
+import { _toast } from '../utils/utils'
 
-import axios from 'axios';
-import NetInfo, { NetInfoState } from "@react-native-community/netinfo"
-import { _toast } from '../utils/utils';
+const qs = require('qs')
 
-const qs = require('qs');
-
-let showFunc: any;
-let hideFunc: any;
-
+let showFunc: any
+let hideFunc: any
 
 export class httpServiceFunc {
-  showFunction = (_callBack: any) => {
-    showFunc = _callBack;
-  };
-  hideFunction = (_callBack: any) => {
-    hideFunc = _callBack;
-  };
+    showFunction = (_callBack: any) => {
+        showFunc = _callBack
+    }
+    hideFunction = (_callBack: any) => {
+        hideFunc = _callBack
+    }
 }
 
-
-
 const http = axios.create({
-  baseURL: "http://template-demo.aspnetboilerplate.com",
-  timeout: 30000,
-  paramsSerializer: function (params) {
-    return qs.stringify(params,{
-      encode: false,
-    });
-  },
-});
+    baseURL: 'http://template-demo.aspnetboilerplate.com',
+    timeout: 30000,
+    paramsSerializer: function(params) {
+        return qs.stringify(params, {
+            encode: false,
+        })
+    },
+})
 
 http.interceptors.request.use(
-  function (config) {
-    showFunc();
-    
-    //  if (!!abp.auth.getToken()) {
-    //    config.headers.common['Authorization'] = 'Bearer ' + abp.auth.getToken();
-    //  }
-   
-    //   config.headers.common['.AspNetCore.Culture'] = abp.utils.getCookieValue('Abp.Localization.CultureName');
-    //   config.headers.common['Abp.TenantId'] = abp.multiTenancy.getTenantIdCookie();
-    //config.headers.common['Abp.TenantId'] = 1;
-    return config;
-  },
-  function (error) {
+    function(config) {
+        showFunc()
 
-    return Promise.reject(error);
-  }
-);
+        //  if (!!abp.auth.getToken()) {
+        //    config.headers.common['Authorization'] = 'Bearer ' + abp.auth.getToken();
+        //  }
+
+        //   config.headers.common['.AspNetCore.Culture'] = abp.utils.getCookieValue('Abp.Localization.CultureName');
+        //   config.headers.common['Abp.TenantId'] = abp.multiTenancy.getTenantIdCookie();
+        //config.headers.common['Abp.TenantId'] = 1;
+        return config
+    },
+    function(error) {
+        return Promise.reject(error)
+    },
+)
 
 http.interceptors.response.use(
-  response => {
-    hideFunc();
-    return response;
-  },
-  error => {
-    hideFunc();
-    if (!!error.response && !!error.response.data.error && !!error.response.data.error.message && error.response.data.error.details) {
-      _toast(error.response.data.error.details);
-    } else if (!!error.response && !!error.response.data.error && !!error.response.data.error.message) {
-      _toast(error.response.data.error.message);
-    } else if (!error.response) {
-      _toast(error.response);
-    }
-    NetInfo.fetch('ethernet').then((e: NetInfoState) => {
-      if (e.isConnected == false) {
-        _toast("Connectionlost");
+    response => {
+        hideFunc()
+        return response
+    },
+    error => {
+        hideFunc()
+        if (
+            !!error.response &&
+            !!error.response.data.error &&
+            !!error.response.data.error.message &&
+            error.response.data.error.details
+        ) {
+            _toast(error.response.data.error.details)
+        } else if (
+            !!error.response &&
+            !!error.response.data.error &&
+            !!error.response.data.error.message
+        ) {
+            _toast(error.response.data.error.message)
+        } else if (!error.response) {
+            _toast(error.response)
+        }
+        NetInfo.fetch('ethernet').then((e: NetInfoState) => {
+            if (e.isConnected == false) {
+                _toast('Connectionlost')
+            }
+        })
 
-      }
-    })
+        return Promise.reject(error)
+    },
+)
 
-    return Promise.reject(error);
-  }
-);
-
-
-
-
-
-export default http;
+export default http
