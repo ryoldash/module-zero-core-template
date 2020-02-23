be68149319bd83fc049dde53bd47490bfc6898b4
Rashiduddin Yoldash <ryoldash@hotmail.com>
2020-02-23 21:03:42 +0000

    Updated package versions

--- react-native/src/App.tsx
+++ react-native/src/App.tsx
@@ -8,44 +8,28 @@
  * @format
  */
 
-import React from 'react';
-import { Provider } from 'mobx-react';
-import initializeStores from './stores/storeInitializer';
-import RoutingContainer from './components/routing/routingConfig';
-
-
-
+import React from "react";
+import { Provider } from "mobx-react";
+import initializeStores from "./stores/storeInitializer";
+import RoutingContainer from "./components/routing/routingConfig";
 
 const stores = initializeStores();
 console.disableYellowBox = true;
-interface Props {
-
-}
+interface Props {}
 interface State {
-  appState:any
+  appState: any;
 }
 
-export default class App extends React.Component<Props,State> {
+export default class App extends React.Component<Props, State> {
   constructor(props) {
     super(props);
   }
 
-
   render() {
-    
     return (
       <Provider {...stores}>
-         <RoutingContainer/>
+        <RoutingContainer />
       </Provider>
     );
   }
 }
-
-
-
-
-
-
-
-
-
--- react-native/src/models/Login/loginModel.ts
+++ react-native/src/models/Login/loginModel.ts
@@ -1,12 +1,8 @@
-import { observable } from 'mobx';
-
 class LoginModel {
   //tenancyName!: string;
   userNameOrEmailAddress: string;
   password: string;
-  rememberMe:boolean;
-  
-  
+  rememberMe: boolean;
 }
 
 export default LoginModel;
